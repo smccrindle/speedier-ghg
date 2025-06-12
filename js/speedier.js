2b2609220ddf3827de91aab4711939c90e0caeee
Scott McCrindle <iainmccrindle@gmail.com>
2025-06-12 12:27:40 -0400

    Modified some UI wording and removed some JS comments

--- js/speedier.js
+++ js/speedier.js
@@ -201,35 +201,13 @@ function newChart(jsonFileName) {
               intersect: true,
               callbacks: {
                 afterLabel: function(context) {
-
-                  // console.log("--- DEBUGGING TOOLTIP COMMENT ---");
-                  //   console.log("Full context object:", context); // Inspect this thoroughly
-                  //   console.log("context.raw BEFORE IF:", context.raw); // This should be the full data point object
-                  //   console.log("Type of context.raw:", typeof context.raw);
-
                     const dataPoint = context.raw;
-
                     if (dataPoint) { // Check if dataPoint itself is valid/exists
-                        // console.log("dataPoint exists.");
-                        // console.log("Does dataPoint have a 'comment' property?", 'comment' in dataPoint); // Check for existence
-                        // console.log("Value of dataPoint.comment:", dataPoint.comment); // Check its value
-                        // console.log("Type of dataPoint.comment:", typeof dataPoint.comment);
-                        // console.log("Is dataPoint.comment truthy (not null/undefined/empty string)?", !!dataPoint.comment);
-
                         if (dataPoint.comment) { // Check if comment has a value
-                            // console.log("Comment is truthy, will return:", 'Note: ' + dataPoint.comment);
                             return 'Note: ' + dataPoint.comment;
                         }
                     }
-
-                    // console.log("No comment or comment property missing (returning empty string).");
                     return '';
-
-                  // const dataPoint = context.raw; // This gets the original data point object ({x, y, comment})
-                  // if (dataPoint && dataPoint.comment) {
-                  //     return 'Note: ' + dataPoint.comment; // Return the comment text
-                  // }
-                  // return ''; // Return an empty string if no comment for this data point
                 }
               }
             }
